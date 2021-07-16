024993b668a1b8091dbc6fbe1ae7fe700fa5571c
Mirren <meditos@gmail.com>
2021-07-16 18:52:54 +0200

    Infrastructure scripts.

---
+++ scripts/start_app.sh
@@ -0,0 +1,1 @@
+java --enable-preview -jar ../target/neo4j-api-0.0.1-SNAPSHOT.jar --org.neo4j.driver.uri=bolt://localhost:7687 --org.neo4j.driver.authentication.password=secret
---
+++ scripts/start_server.sh
@@ -0,0 +1,1 @@
+docker run --name testneo4j --publish=7474:7474 --publish=7687:7687 -e 'NEO4J_AUTH=neo4j/secret' neo4j:4.0.2
